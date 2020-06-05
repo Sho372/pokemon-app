b64fe91619b716a5e0185c9de2ecb71c28c1c74d
Sungmin Cho <abelsteiger@gmail.com>
2020-06-05 11:55:03 -0700

    add: model (Team, History)

---
+++ pokemon-app/Model/Team.swift
@@ -0,0 +1,14 @@
+//
+//  Team.swift
+//  pokemon-app
+//
+//  Created by Richard Cho on 2020-06-05.
+//  Copyright © 2020 user169300. All rights reserved.
+//
+
+import Foundation
+
+struct Team {
+    var name: String
+    var party: [Pokemon]
+}
