3931ad77920da8c188bb5a24a74b1cfc5782e196
Shohei.Mizuno <32non23ca@gmail.com>
2020-06-08 14:42:11 -0700

    WIP add ManagedTeam

---
+++ pokemon-app/Model/Team.swift
@@ -0,0 +1,26 @@
+//
+//  Team.swift
+//  pokemon-app
+//
+//  Created by user169300 on 6/4/20.
+//  Copyright © 2020 user169300. All rights reserved.
+//
+
+import Foundation
+
+struct Teams: Codable {
+    let results: [Team]
+}
+
+struct Team: Codable {
+    let teamName: String
+    let pokemonName1: String
+    let pokemonName2: String
+    let pokemonName3: String
+    let pokemonName4: String
+    let pokemonName5: String
+    let pokemonName6: String
+    let isArchive: Bool
+    let createdAt: Date
+    let updatedAt: Date
+}
