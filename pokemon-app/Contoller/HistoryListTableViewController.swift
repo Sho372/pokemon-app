3398f347502209c8473cf94d7a904db0f1b0eef2
mk1990h2 <mikass111@yahoo.co.jp>
2020-06-09 16:29:36 -0700

    Implement HistoryListTableViewController except for data part

--- pokemon-app/Contoller/HistoryListTableViewController.swift
+++ pokemon-app/Contoller/HistoryListTableViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class HistoryListTableViewController: UITableViewController {
     
     private let cellId = "HistoryCell"
+    
     // MARK: Fix this after implementing CoreData part
     var historyList: [History] = [
         History(date: Date(), season: "June", party: [Pokemon(name: "Pikachu")], isChosen: [true], battleResult: true),
@@ -31,65 +32,28 @@ class HistoryListTableViewController: UITableViewController {
         return historyList.count
     }
 
+    // MARK: Fix this after implementing CoreData part
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! HistoryTableViewCell
         let history = historyList[indexPath.row]
         
         let formatter = DateFormatter()
+        formatter.dateStyle = .medium
         cell.dateLabel.text = formatter.string(from: history.date)
+        cell.opponentLabel1.text = history.party[0].name
         
         if history.battleResult {
-            cell.resultImage.image = UIImage(named: "circle")
+            cell.resultImage.image = UIImage(systemName: "circle")
         } else {
-            cell.resultImage.image = UIImage(named: "multiply")
+            cell.resultImage.image = UIImage(systemName: "multiply")
         }
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toHistoryDetail" {
+            let detailView = segue.destination as! HistoryDetailViewController
         }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
