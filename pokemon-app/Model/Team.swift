2049ba5ff84aad33bdeea4cd42dfac803c3bd2c1
Mika <49261824+mk1990h2@users.noreply.github.com>
2020-06-09 22:31:12 -0700

    Revert "Sho managed team"

--- pokemon-app/Model/Team.swift
+++ pokemon-app/Model/Team.swift
@@ -2,25 +2,13 @@
 //  Team.swift
 //  pokemon-app
 //
-//  Created by user169300 on 6/4/20.
+//  Created by Richard Cho on 2020-06-05.
 //  Copyright © 2020 user169300. All rights reserved.
 //
 
 import Foundation
 
-struct Teams: Codable {
-    let results: [Team]
-}
-
-struct Team: Codable {
-    let name: String
-    let createdAt: Date
-    let isArchive: Bool
-    let updatedAt: Date
-    let pokemonName1: String
-    let pokemonName2: String?
-    let pokemonName3: String?
-    let pokemonName4: String?
-    let pokemonName5: String?
-    let pokemonName6: String?
+struct Team {
+    var name: String
+    var party: [Pokemon]
 }
