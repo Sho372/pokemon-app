8496d3d9802697c533ec4d569b7c0420d34e93c3
Shohei.Mizuno <32non23ca@gmail.com>
2020-06-10 13:56:26 -0700

    [ManagedHistory] add ManagedHistory, ManagedOppoentPokemon and ManagedSelectedPokemon

---
+++ pokemon-app/Model/ManagedOpponentPokemons.swift
@@ -0,0 +1,14 @@
+//
+//  ManagedOpponentPokemons.swift
+//  pokemon-app
+//
+//  Created by user169300 on 6/10/20.
+//  Copyright © 2020 user169300. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ManagedOpponentPokemons: NSManagedObject {
+
+}
