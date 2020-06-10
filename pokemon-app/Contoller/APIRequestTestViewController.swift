4c5c67e1f2f0be7aa1d076b3041bccecfcb9d54f
Shohei.Mizuno <32non23ca@gmail.com>
2020-06-10 10:41:43 -0700

    [ManagedTeam] add ManagedTeam

--- pokemon-app/Contoller/APIRequestTestViewController.swift
+++ pokemon-app/Contoller/APIRequestTestViewController.swift
@@ -38,16 +38,22 @@ class APIRequestTestViewController: UIViewController {
                 _ = try? ManagedPokemon.findOrCreatePokemon(matching: pokemon, in: context)
             }
             try? context.save()
-            self.printDatabaseStatistics()
+            let firstTeam = Team(name: "first team", createdAt: Date(), isArchive: false, updatedAt: Date(), pokemonName1: "aaa", pokemonName2: "bbb", pokemonName3: "ccc", pokemonName4: "ddd", pokemonName5: "eee", pokemonName6: "fff"
+            )
+            let secondTeam = Team(name: "second team", createdAt: Date(), isArchive: false, updatedAt: Date(), pokemonName1: "aaa", pokemonName2: "bbb", pokemonName3: "ccc", pokemonName4: "ddd", pokemonName5: "eee", pokemonName6: "fff"
+            )
+            try? ManagedTeam.findOrCreateTeam(matching: firstTeam, in: context)
+            try? ManagedTeam.findOrCreateTeam(matching: secondTeam, in: context)
+            try? context.save()
+
+            let count = ManagedTeam.count(in: context)
+                print("\(count)teams" )
+            let teams = ManagedTeam.fetchAll(in: context)
+            for team in teams {
+                print(team.name)
+                print(team.pokemonName1)
+                print(team.createdAt)
+            }
         }
-        
     }
-    
-    private func printDatabaseStatistics(){
-        let context = container?.viewContext
-        if let pokemonCount = (try? context?.fetch(ManagedPokemon.fetchRequest()))?.count{
-            print("\(pokemonCount) pokemons")
-        }
-    }
-    
 }
